21fd3800c9fc827b97a2512d1e6683622b766edb
Steve Higham (MSI Ubuntu) <steve@sjlt.co.uk>
2022-10-06 21:29:26 +0100

    Running on node 12

--- server/run.js
+++ server/run.js
@@ -1,22 +1,23 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 import http from 'http';
 import app from './server';
 const port = normalizePort(process.env.PORT || '3000');
 
 // Set port for app
-app.set('port', port);
+app.set ('port', port);
 
 // Create the server
-const server = http.createServer(app);
+const server = http.createServer (app);
 
 // Listen...and we're off!
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen (port);
+server.on ('error', onError);
+server.on ('listening', onListening);
 
-function normalizePort(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
+function normalizePort (val)
+{
+    var port = parseInt (val, 10);
+    if (isNaN (port)) {
         // named pipe
         return val;
     }
@@ -27,27 +28,29 @@ function normalizePort(val) {
     return false;
 }
 
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+function onError (error)
+{
+    if (error.syscall !== 'listen') { throw error; }
     var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
     // handle specific listen errors with friendly messages
     switch (error.code) {
         case 'EACCES':
-            console.error(`${bind} requires elevated privileges`);
-            process.extest(1);
+            console.error (`${bind} requires elevated privileges`);
+            process.extest (1);
             break;
         case 'EADDRINUSE':
-            console.error(`${bind} is already in use`);
-            process.extest(1);
+            console.error (`${bind} is already in use`);
+            process.extest (1);
             break;
         default:
             throw error;
     }
 }
 
-function onListening() {
+function onListening ()
+{
     const addr = server.address();
     console.info(`==> 🌎  Newsfeed is running on port ${addr.port} ✅`);
 }
+
+// End of run.js
