d0123fe888343acea62fc3ff094342b89cf3491d
Steve Higham (MSI Ubuntu) <steve@sjlt.co.uk>
2022-10-07 18:18:10 +0100

    Chapter 4 complete.

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -10,56 +10,59 @@ import Comments from "../comment/Comments";
 import UserHeader from "../post/UserHeader";
 import Loader from "../Loader";
 
-export class Post extends Component {
+export class Post extends Component
+{
     static propTypes = {
-        post: PropTypes.shape({
-            comments: PropTypes.array,
-            content: PropTypes.string,
-            date: PropTypes.number,
-            id: PropTypes.string.isRequired,
-            image: PropTypes.string,
-            likes: PropTypes.array,
-            location: PropTypes.object,
-            user: PropTypes.object,
-            userId: PropTypes.string
-        })
+        post: PropTypes.shape ({
+                                   comments:    PropTypes.array,
+                                   content:     PropTypes.string,
+                                   date:        PropTypes.number,
+                                   id:          PropTypes.string.isRequired,
+                                   image:       PropTypes.string,
+                                   likes:       PropTypes.array,
+                                   location:    PropTypes.object,
+                                   user:        PropTypes.object,
+                                   userId:      PropTypes.string
+                               })
     };
-    constructor(props) {
-        super(props);
+
+    constructor (props)
+    {
+        super (props);
         this.state = {
             post: null,
             comments: [],
             showComments: false,
             user: this.props.user
         };
-        this.loadPost = this.loadPost.bind(this);
+        this.loadPost = this.loadPost.bind (this);
     }
-    componentDidMount() {
-        this.loadPost(this.props.id);
+
+    componentDidMount () { this.loadPost (this.props.id); }
+
+    loadPost (id)
+    {
+        API.fetchPost (id)
+           .then (res => res.json ())
+           .then (post => { this.setState (() => ({ post })); });
     }
-    loadPost(id) {
-        API.fetchPost(id)
-            .then(res => res.json())
-            .then(post => {
-                this.setState(() => ({ post }));
-            });
-    }
-    render() {
-        if (!this.state.post) {
-            return <Loader />;
-        }
+
+    render ()
+    {
+        if (! this.state.post) { return <Loader />; }
         return (
-            <div className="post">
-                <UserHeader date={this.state.post.date} user={this.state.post.user} />
-                <Content post={this.state.post} />
-                <Image post={this.state.post} />
-                <Link link={this.state.post.link} />
-                <PostActionSection showComments={this.state.showComments} />
+            <div className = "post">
+                <UserHeader date = { this.state.post.date }
+                            user= { this.state.post.user } />
+                <Content post={ this.state.post } />
+                <Image post={ this.state.post } />
+                <Link link={ this.state.post.link } />
+                <PostActionSection showComments={ this.state.showComments } />
                 <Comments
-                    comments={this.state.comments}
-                    show={this.state.showComments}
-                    post={this.state.post}
-                    user={this.props.user}
+                    comments = {    this.state.comments }
+                    show = {        this.state.showComments }
+                    post = {        this.state.post }
+                    user = {        this.props.user }
                 />
             </div>
         );
@@ -67,3 +70,5 @@ export class Post extends Component {
 }
 
 export default Post;
+
+// End of Post.js
